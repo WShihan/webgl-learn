cc3d86398d978205ea93ff3fd0e8800c8eb7c8b6
wsh <3443327820@qq.com>
2023-04-04 16:10:55 +0800

    new:chapter1 code run successfully at first time

---
+++ customUtils.js
@@ -0,0 +1,25 @@
+function createShader(gl, type, source) {
+  var shader = gl.createShader(type); // 创建着色器对象
+  gl.shaderSource(shader, source); // 提供数据源
+  gl.compileShader(shader); // 编译 -> 生成着色器
+  var success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  console.log(gl.getShaderInfoLog(shader));
+  if (success) {
+    return shader;
+  }
+  gl.deleteShader(shader);
+}
+
+function createProgram(gl, vertexShader, fragmentShader) {
+  var program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  var success = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (success) {
+    return program;
+  }
+
+  console.log(gl.getProgramInfoLog(program));
+  gl.deleteProgram(program);
+}
